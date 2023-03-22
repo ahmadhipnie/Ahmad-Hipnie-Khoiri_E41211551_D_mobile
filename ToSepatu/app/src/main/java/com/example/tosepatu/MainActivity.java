d0346d25c100f4e84d533d12a692fd88e1c74bea
ahmadhipnie <112534396+ahmadhipnie@users.noreply.github.com>
2023-03-22 21:46:55 +0700

    minggu 6 acara 25

--- ToSepatu/app/src/main/java/com/example/tosepatu/MainActivity.java
+++ ToSepatu/app/src/main/java/com/example/tosepatu/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.riwayat_layout);
+        setContentView(R.layout.splash_layout);
     }
 }
